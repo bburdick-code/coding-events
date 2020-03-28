f92ef074edf579fd9f61b1addb43baeba395679e
benjiboy <ben.burdick91@gmail.com>
2020-03-28 13:00:26 -0500

    Added create, edit, and display functions

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,69 @@
+package org.launchcode.codingevents.controllers;
+
+import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.models.Event;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        model.addAttribute("title", "All Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/index";
+    }
+
+    @GetMapping("create")
+    public String renderCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
+        return "events/create";
+    }
+
+    @PostMapping("create")
+    public String createEvent(@ModelAttribute Event newEvent)  {
+        EventData.add(newEvent);
+        return "redirect:";
+    }
+
+    @GetMapping("delete")
+    public String renderDeleteForm(Model model) {
+        model.addAttribute("title","Delete Event");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String deleteEvent(@RequestParam(required = false) int[] eventIds) {
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+        return "redirect:";
+    }
+
+    @GetMapping("edit/{eventId}")
+    public String renderEditForm(Model model, @PathVariable int eventId) {
+        // code
+        Event current = EventData.getById(eventId);
+        model.addAttribute("title", "Edit Event " + current.getName() + " (id=" + current.getId() + ")");
+        model.addAttribute("event", EventData.getById(eventId));
+        return "events/edit";
+    }
+
+    @PostMapping("edit")
+    public String editEvent(@RequestParam int eventId, @RequestParam String name, @RequestParam String description) {
+        //code
+        Event current = EventData.getById(eventId);
+        current.setName(name);
+        current.setDescription(description);
+        return "redirect:";
+    }
+}
---
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -0,0 +1,55 @@
+package org.launchcode.codingevents.models;
+
+import java.util.Objects;
+
+public class Event {
+
+    static private int idCount = 1;
+    private int id;
+
+    private String name;
+    private String description;
+
+    public Event(String name, String description) {
+        this.id = idCount;
+        idCount++;
+        this.name = name;
+        this.description = description;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
